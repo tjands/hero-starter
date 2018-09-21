551b75aad9b3a145e81fba874eee2747a44353e3
Tim Andersen <tim.j.andersen@gmail.com>
2018-09-21 14:19:18 +1000

    carefulassasin

--- hero.js
+++ hero.js
@@ -112,7 +112,7 @@ var moves = {
     // This hero will attempt to kill the closest weaker enemy hero.
     carefulAssassin: function (gameData, helpers) {
         var myHero = gameData.activeHero;
-        if (myHero.health < 50) {
+        if (myHero.health < 60) {
             return helpers.findNearestHealthWell(gameData);
         } else {
             return helpers.findNearestWeakerEnemy(gameData);
@@ -180,7 +180,7 @@ var moves = {
 };
 
 // Set our hero's strategy
-var move =  moves.aggressor;
+var move =  moves.carefulAssassin;
 
 // Export the move function here
 module.exports = move;
